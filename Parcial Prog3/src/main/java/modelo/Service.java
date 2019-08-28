d6090a51e4a9ed0e1a5fd5a6341581371c251a66
rodrigo <rodrigo.oscar.alarcon@gmail.com>
2019-08-28 19:48:33 -0300

    segundo commmit parcial 1

--- Parcial Prog3/src/main/java/modelo/Service.java
+++ Parcial Prog3/src/main/java/modelo/Service.java
@@ -1,5 +1,70 @@
 package modelo;
 
+import java.time.LocalDate;
+
+import excepciones.ServiceIncompletoException;
+
 public class Service {
+	
+	private int idServicio;
+	private Vehiculo elVehiculo;
+	private Cliente elCliente;
+	private LocalDate fechaServicio;
+	private String detalle;
+	private float precio;
+	
+	public Service(int idServicio, Vehiculo elVehiculo, Cliente elCliente, LocalDate fechaServicio, String detalle
+			, float precio) {
+		
+		this.idServicio = idServicio;
+		this.elVehiculo = elVehiculo;
+		this.elCliente = elCliente;
+		this.fechaServicio = fechaServicio;
+		this.detalle = detalle;
+		this.precio = precio;
+	}
+	
+	public static Service factoryService(int idServicio, Vehiculo elVehiculo, Cliente elCliente, LocalDate fechaServicio, 
+			String detalle, float precio) throws ServiceIncompletoException {
+		
+		if(idServicio == 0 || elVehiculo == null || elCliente== null) {
+			throw new ServiceIncompletoException();
+		}else {
+			return new Service(idServicio, elVehiculo, elCliente, fechaServicio, detalle, precio);
+		}
+		
+	}
+
+
+	public int getIdServicio() {
+		return idServicio;
+	}
+
+	public Vehiculo getElVehiculo() {
+		return elVehiculo;
+	}
+
+	public Cliente getElCliente() {
+		return elCliente;
+	}
+
+	public LocalDate getFechaServicio() {
+		return fechaServicio;
+	}
+
+	public String getDetalle() {
+		return detalle;
+	}
+
+	public float getPrecio() {
+		return precio;
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
